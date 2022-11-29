c76270c413b73e74d24a4b6c56ad914bdca7729e
Your Name <you@example.com>
2022-11-29 17:23:54 +0530

    y

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "ci cd pipeline has been established, done"
+    return "ci cd pipeline has been established, done, ok"
 
 
 if __name__ == ('__main__'):

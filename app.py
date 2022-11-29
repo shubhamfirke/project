3ce95c5aed13dc042dd23773d797f480c1e292c4
Your Name <you@example.com>
2022-11-29 14:41:12 +0530

    updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "starting machiene learning project"
+    return "ci cd pipeline has been established"
 
 
 if __name__ == ('__main__'):

048d637e9e8d859bbaea25b731d56638e9251cd9
Your Name <you@example.com>
2022-11-30 15:34:43 +0530

    logger and exception updated

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from housing.logger import logging
+
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    logging.info("we are testing logging module")
     return "ci cd pipeline has been established, done, ok, final"
 
 

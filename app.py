b36d9a03068dc7255ed2917d73133181f25c5add
Your Name <you@example.com>
2022-12-07 21:38:57 +0530

    ok

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask
-from housing.logger import logging
-
+# from housing.logger import logging
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    logging.info("we are testing logging module")
-    return "ci cd pipeline has been established, done, ok, final"
-
+    # logging.info("we are testing logging module")
+    return "ci cd pipeline has been established, done, ok, final, now its done"
 
 if __name__ == ('__main__'):
         app.run(debug=True)

f678894b01a599356af1ddf7ba0692f34999e711
Your Name <you@example.com>
2022-12-10 16:58:54 +0530

    yaml update

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-# from housing.logger import logging
+from housing.logger import logging
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    # logging.info("we are testing logging module")
+    logging.info("we are testing logging module")
     return "ci cd pipeline has been established, done, ok, final, now its done"
 
 if __name__ == ('__main__'):
